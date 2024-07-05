4815fbe2acbbaf2b009bda9ca36d97eff1bed405
Vladimir Lidzhigoryaev <amoureflare@gmail.com>
2024-07-05 23:47:06 +0400

    Обработка пустого слова

--- src/main/java/com/amoureflare/model/Word.java
+++ src/main/java/com/amoureflare/model/Word.java
@@ -4,10 +4,17 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 
-@RequiredArgsConstructor
 public class Word {
     @Getter
     private final String word;
+
+    public Word(String word) {
+        if (StringUtils.isEmpty(word.trim())) {
+            throw new IllegalArgumentException("Empty word");
+        } else {
+            this.word = word;
+        }
+    }
 
     public boolean isEmpty() {
         return StringUtils.isEmpty(word);
